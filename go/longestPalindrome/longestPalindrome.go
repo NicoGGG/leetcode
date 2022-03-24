2a292f0daa9d316572123ccc9d7f905edda150b7
NicoGGG <coolranger@protonmail.com>
2022-03-24 16:09:38 +0100

    isPalindrome ok

--- go/longestPalindrome/longestPalindrome.go
+++ go/longestPalindrome/longestPalindrome.go
@@ -3,3 +3,13 @@ package longestpalindrome
 func longestPalindrome(s string) string {
 	return ""
 }
+
+func isPalindrome(s string) bool {
+	l := len(s)
+	for i, c := range s {
+		if byte(c) != s[l-1-i] {
+			return false
+		}
+	}
+	return true
+}
--- go/longestPalindrome/longestPalindrome_test.go
+++ go/longestPalindrome/longestPalindrome_test.go
@@ -5,6 +5,42 @@ import (
 	"testing"
 )
 
+func TestIsPalindrome1(t *testing.T) {
+	i := "aba"
+	e := true
+	r := isPalindrome(i)
+	if e != r {
+		log.Fatalf("Expected result: %v, got %v", e, r)
+	}
+}
+
+func TestIsPalindrome2(t *testing.T) {
+	i := "ab"
+	e := false
+	r := isPalindrome(i)
+	if e != r {
+		log.Fatalf("Expected result: %v, got %v", e, r)
+	}
+}
+
+func TestIsPalindrome3(t *testing.T) {
+	i := "bb"
+	e := true
+	r := isPalindrome(i)
+	if e != r {
+		log.Fatalf("Expected result: %v, got %v", e, r)
+	}
+}
+
+func TestIsPalindrome4(t *testing.T) {
+	i := ""
+	e := true
+	r := isPalindrome(i)
+	if e != r {
+		log.Fatalf("Expected result: %v, got %v", e, r)
+	}
+}
+
 func TestLongestPalindrome1(t *testing.T) {
 	s := "babad"
 	r := longestPalindrome(s)
